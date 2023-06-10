a1d13747ee8428c0d19b5c7f29717b3aec65f28b
Ulises Capistrán <ucapistranmtz@gmail.com>
2023-06-10 07:40:29 -0500

    lesson 83 https://www.udemy.com/course/react-cero-experto/learn/lecture/19758044\#overview

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,7 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
+import { useState, useEffect } from 'react';
 import { getGifs } from '../helpers/getGifs';
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [counter, setcounter] = useState(10);
+
+  useEffect(() => {
+    getGifs(category);
+  }, []);
+
   return (
     <>
       <h3>{category}</h3>
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async category => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=tbq5Isc9VmXLeaIxWiw4KHNLFIrGHJV9&q=${category}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=tbq5Isc9VmXLeaIxWiw4KHNLFIrGHJV9&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data = [] } = await resp.json();
   const images = data.map(item => ({
