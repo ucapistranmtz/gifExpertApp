a17a61dc2c6158c43877e95ec7379f4b18a107ee
Ulises Capistrán <ucapistranmtz@gmail.com>
2023-06-10 07:07:23 -0500

    lesson 82 https://www.udemy.com/course/react-cero-experto/learn/lecture/19756276\#overview

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,7 @@ import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+  const [categories, setCategories] = useState(['One Punch']);
 
   const onAddCategory = newCategory => {
     if (categories.includes(newCategory)) return;
--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
-
+import { getGifs } from '../helpers/getGifs';
 export const GifGrid = ({ category }) => {
+  getGifs(category);
   return (
     <>
       <h3>{category}</h3>
