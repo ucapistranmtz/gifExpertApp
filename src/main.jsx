5ff463d8612fde031b38868fc327f9dfa28cbcf6
Ulises Capistrán <ucapistranmtz@gmail.com>
2023-06-14 07:34:50 -0500

    gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom/client';
 
 import { GifExpertApp } from './GifExpertApp.jsx';
 import './styles.css';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GifExpertApp />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<GifExpertApp />);
